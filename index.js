cdbb81d79feafc7c1c929c8c1460ed396b0a91ad
jaredleedev <jaredlee.dev@gmail.com>
2019-10-06 15:37:38 -0500

    Adds transition div functions.

--- index.js
+++ index.js
@@ -1,4 +1,49 @@
+// Global References to page elements.
 
-$("#checkItOutButton").click(function() {
-    
-  });
+// Buttons:
+var checkItOutButton = $("#checkItOutButton");
+
+// Divs:
+var coverDiv = $("#coverDiv");
+var webDiv = $("#webDiv");
+var animationDiv = $("#animationDiv");
+var resumeDiv = $("#resumeDiv");
+var contactDiv = $("contactDiv");
+var aboutMeDiv = $("#aboutMeDiv");
+
+// Div currently showing:
+var currentDiv = coverDiv;
+
+// Hides cover div and shows Web Design/Programming div.
+checkItOutButton.click(function() {
+    // Hide cover and show webDiv.
+    transitionDiv(webDiv);
+});
+
+// Transitions from current div to desired div.
+function transitionDiv(newDiv) {
+    hideElement(currentDiv);
+    showElement(newDiv);
+    updateCurrentDiv(newDiv);
+}
+
+// Sets given element display property to none.
+function hideElement(elem) {
+    elem.prop("display", "none");
+}
+
+// Sets given element display property to visible.
+function showElement(elem) {
+    elem.prop("display", "visible");
+}
+
+// Updates currentDiv to given div.
+function updateCurrentDiv(newDiv) {
+    if (newDiv != undefined) {
+        currentDiv = newDiv;
+    }
+
+    else {
+        console.log("Error: newDiv is undefined!");
+    }
+}
